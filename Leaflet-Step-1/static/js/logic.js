efae95d36477dd6bd68bb7c6bd05d526621b539b
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-06-03 01:51:09 -0500

    final

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -27,6 +27,21 @@ function createMap(earthquakes){
   });
 
   L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend"), 
+      magnitudeLevels = [0,-10,10,30,50,70];
+  
+      div.innerHTML += "<h3>Magnitude</h3>"
+  
+      /*for (var i = 0; i < magnitudeLevels.length; i++) {
+          div.innerHTML +=
+              '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
+              magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
+      }*/
+      return div;
+  }
+  legend.addTo(myMap);
 
 }
 
@@ -51,7 +66,7 @@ function markerSize(response){
       fillColor: chooseColor(response.features[i].geometry.coordinates[2]),
       // Adjust radius
       radius: markerSize(response.features[i].properties.mag)
-    }).bindPopup("<h1> Magnitud: " + response.features[i].properties.mag + "</h1> <hr> <h3>Depth in Km: " + response.features[i].geometry.coordinates[2]+ "</h3> <h3>Location: "+ response.features[i].properties.place+"</h3>");
+    }).bindPopup("<h1> Magnitude: " + response.features[i].properties.mag + "</h1> <hr> <h3>Depth in Km: " + response.features[i].geometry.coordinates[2]+ "</h3> <h3>Location: "+ response.features[i].properties.place+"</h3>");
     earthquakeMarker.push(circle)
 
     //console.log(response.features[i].geometry.coordinates[0])
@@ -111,76 +126,3 @@ function markerSize(response){
   
 }
 
-/*
-  // setting the magnitiud of the EQ as the zise for the markers
-  function markerSize(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-    return magnitude * 4;
-  }
-  //define attriviutes for the marker
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: chooseColor(feature.properties.mag),
-      color: "black",
-      radius: markerSize(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-  }
-  //fuction that defines the color based on the mag 
-  function chooseColor(magnitude) {
-    switch (true) {
-    case magnitude > 5:
-        return "#581845";
-    case magnitude > 4:
-        return "#900C3F";
-    case magnitude > 3:
-        return "#C70039";
-    case magnitude > 2:
-        return "#FF5733";
-    case magnitude > 1:
-        return "#FFC300";
-    default:
-        return "#DAF7A6";
-    }
-  }
-
-    // Create a GeoJSON Layer Containing the Features Array on the earthquakeData Object
-    L.geoJSON(earthquakeData, {
-      pointToLayer: function(feature, latlng) {
-          return L.circleMarker(latlng);
-      },
-      style: styleInfo,
-      // Function to Run Once For Each feature in the features Array
-      // Give Each feature a Popup Describing the Place & Time of the Earthquake
-      onEachFeature: function(feature, layer) {
-          layer.bindPopup("<h4>Location: " + feature.properties.place + 
-          "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
-          "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-      }
-    // Add earthquakeData to earthquakes LayerGroups 
-  }).addTo(earthquakes);
-      // Add earthquakes Layer to the Map
-  earthquakes.addTo(myMap);
-
-  // Set Up Legend
-  var legend = L.control({ position: "bottomright" });
-  legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend"), 
-      magnitudeLevels = [0, 1, 2, 3, 4, 5];
-
-      div.innerHTML += "<h3>Magnitude</h3>"
-
-      for (var i = 0; i < magnitudeLevels.length; i++) {
-          div.innerHTML +=
-              '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
-              magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
-      }
-      return div;
-  };
-  // Add Legend to the Map
-  legend.addTo(myMap);*/
